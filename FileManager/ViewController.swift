ad280f5863a92b526d9fa91fa23ef94f464b530f
Дмитрий Снигирев <dmitry@iMac-Dmitrij.local>
2024-01-06 15:06:19 +0400

    initial commit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Дмитрий Снигирев on 06.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
